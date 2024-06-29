1de9458fc7e7f4de4bfce54ef26d13a5d076fa23
carisaelam <carisaelam@gmail.com>
2024-06-29 07:36:18 -0400

    Write tests

--- cipher.rb
+++ cipher.rb
@@ -1,32 +1,38 @@
 # frozen_string_literal: true
 
-def cipher(string, shift)
+class CaesarCipher
   # turns each char of string into its ordinal number
-  ord_array = string.downcase.chars.map(&:ord)
-  p "ord_array: #{ord_array}"
+  def ord_array(string)
+    string.downcase.chars.map(&:ord)
+  end
 
   # adjust each char ord number by the shift factor
-  adjusted_ord_array = ord_array.map do |char_code|
-    p "char code: #{char_code}"
-    case char_code
+  def adjusted_ord_array(ord_array, shift)
+    ord_array.map do |char_code|
+      case char_code
 
-    # a=97 z=122; ensures only a-z
-    when 97..122
-      new_char_code = char_code + shift
-      new_char_code -= 26 while new_char_code > 122 # loops to a after z
-      new_char_code
+        # a=97 z=122; ensures only a-z
+      when 97..122
+        new_char_code = char_code + shift
+        new_char_code -= 26 while new_char_code > 122 # loops to a after z
+        new_char_code
 
-    # for characters not a-z like (.), return that same char
-    else
-      char_code
+      # for characters not a-z like (.), return that same char
+      else
+        char_code
+      end
     end
   end
 
-  adjusted_ord_array.map(&:chr).join
+  # shifts a string by (shift) digits according to caesar cipher
+  def cipher(string, shift)
+    ord_array = ord_array(string)
+    adjusted_ord_array = adjusted_ord_array(ord_array, shift)
+    adjusted_ord_array.map(&:chr).join
+  end
 end
 
 # Use cases
-p cipher('abcd..z', 2)
-
-# p cipher('I will shift 4 characters', 4) # => "m ampp wlmjx 4 glevegxivw"
-# p cipher('I only change letters (@#*!', 9) # => ""r xwuh lqjwpn unccnab (@#*!"
+example = CaesarCipher.new
+p example.cipher('I will shift 4 characters', 4) # => "m ampp wlmjx 4 glevegxivw"
+p example.cipher('I only change letters (@#*!', 9) # => ""r xwuh lqjwpn unccnab (@#*!"
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -1,7 +1,41 @@
 require_relative '../cipher'
 
-RSpec.describe '#cipher' do
-  it 'exists' do
-    expect(cipher('I will shift 4 characters', 4)).to eql('m ampp wlmjx 4 glevegxivw')
+RSpec.describe CaesarCipher do
+  describe '#ord_array' do
+    it 'turns each character into an ordinal number' do
+      expect(subject.ord_array('abc')).to eql([97, 98, 99])
+    end
+
+    it 'downcases letters before converting to ordinal' do
+      expect(subject.ord_array('ABC')).to eql([97, 98, 99])
+    end
+
+    it 'works for a single character string' do
+      expect(subject.ord_array('a')).to eql([97])
+    end
+  end
+
+  describe '#adjusted_ord_array' do
+    it 'shifts each char ord number by shift factor' do
+      expect(subject.adjusted_ord_array([97, 98, 99], 1)).to eql([98, 99, 100])
+    end
+
+    it 'works for a single character array' do
+      expect(subject.adjusted_ord_array([97], 1)).to eql([98])
+    end
+
+    it 'only shifts characters a-z' do
+      expect(subject.adjusted_ord_array([96, 123], 1)).to eql([96, 123])
+    end
+
+    it 'loops to a after z' do
+      expect(subject.adjusted_ord_array([122], 1)).to eql([97])
+    end
+  end
+
+  describe '#cipher' do
+    it 'returns a shifted string' do
+      expect(subject.cipher('I will shift 4 characters', 4)).to eql('m ampp wlmjx 4 glevegxivw')
+    end
   end
 end
